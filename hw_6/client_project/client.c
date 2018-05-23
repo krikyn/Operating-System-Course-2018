0311af741f336a25e1963c24dc125c0feac0dff4
Kirill Vakhrushev <kirill.vakhrushev@gmail.com>
2018-05-23 21:31:06 +0300

    Added connect in the client within the epoll loop

--- hw_6/client_project/client.c
+++ hw_6/client_project/client.c
@@ -10,25 +10,9 @@
 #include <printf.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-
-void rm_spaces(void *src);
-
-void rm_slash_n(void *src);
-
-int send_safe(int socket, void *buffer, size_t length);
-
-int get_safe(int socket_fd, char *message);
-
-
-void set_non_block(int fd)
-{
-    int flag = fcntl ( fd, F_GETFL, 0 );
-    fcntl ( fd, F_SETFL, flag | O_NONBLOCK );
-}
-
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
 
     if (argc < 2 || (argc > 1 && strcmp(argv[1], "-help")) == 0) {
         printf("Usage: ./client_project message [host [port]](optional)\n");
@@ -37,7 +21,7 @@ int main(int argc, char** argv)
 
     char message[1000];
 
-    sprintf(message, "%s",argv[1]);
+    sprintf(message, "%s", argv[1]);
     char *host = "127.0.0.1";
     int port = 3030;
 
@@ -67,73 +51,102 @@ int main(int argc, char** argv)
 
     }
 
-
-    int sk = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-
-    struct sockaddr_in sa = {0};
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(port);
-
-    struct sockaddr_in *psa = &sa;
-
-    inet_pton(AF_INET, host, &psa->sin_addr.s_addr);
-
-    if(connect(sk, (struct sockaddr*)&sa, sizeof(sa)) < 0)
-    {
-        printf("Connection failed");
-        close(sk);
-        return 0;
+    int fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, IPPROTO_TCP);
+    if (fd == -1) {
+        perror("create socket");
+        return 1;
     }
 
-    set_non_block(sk);
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    inet_aton(host, &addr.sin_addr);
 
-    int efd;
-    efd = epoll_create(10);
-    if(efd == -1)
-    {
-        perror("Error with epoll create");
-        exit(1);
+    bool connected = true;
+    int i = connect(fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in));
+
+    if ((i == -1) && (errno == EINPROGRESS)) {
+        connected = false;
     }
 
     struct epoll_event event;
-    struct epoll_event events[10];
-    event.events = EPOLLOUT | EPOLLIN | EPOLLET;
-    event.data.fd = sk;
-    epoll_ctl(efd, EPOLL_CTL_ADD, sk, &event);
+    int epollfd;
+    epollfd = epoll_create(10);
 
+    event.events = EPOLLIN;
+    if (!connected) {
+        event.events |= EPOLLOUT;
+    }
+    event.data.fd = fd;
+    int r = epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    if (r == -1) {
+        perror("epoll 1");
+        close(epollfd);
+        close(fd);
+        return 1;
+    }
 
     int loop = 0;
     int epollout = 0;
     int epollin = 0;
-    while(1)
-    {
-        char buffer[1000];
-        int n = 5;
 
-        if(loop == 1)
-        {
+    while (1) {
+        struct epoll_event events[10];
+        int n = epoll_wait(epollfd, events, 10, -1);
+        if (n == -1) {
+            close(epollfd);
+            close(fd);
+            return 1;
+        }
+
+        char buffer[1000];
+
+        if (loop == 1) {
             break;
         }
 
 
-        n = epoll_wait(efd, events, 10, -1);
-        //printf("%d\n", n);
+        for (int i = 0; i < n; i++) {
 
-        for(int i = 0; i < n; i++)
-        {
-            if((events[i].events & EPOLLOUT) && epollout==0)
-            {
+            if (events[i].data.fd == fd) {
+                if (events[i].events & EPOLLOUT) {
+                    if (!connected) {
+                        int err = 0;
+                        socklen_t len = sizeof(int);
+                        int gi = getsockopt(fd, SOL_SOCKET, SO_ERROR, &err, &len);
+                        if (gi != -1) {
+                            if (err == 0) {
+                                struct epoll_event event1;
+                                event1.events = EPOLLIN | EPOLLOUT;
+                                event1.data.fd = fd; // user data
+                                int nn = epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event1);
+                                if (nn == -1) {
+                                    perror("epoll 2");
+                                    close(epollfd);
+                                    close(fd);
+                                    return 1;
+                                }
+                            } else {
+                                struct epoll_event event1;
+                                event1.events = 0;
+                                event1.data.fd = fd;
+                                epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, &event1);
+                                close(fd);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if ((events[i].events & EPOLLOUT) && epollout == 0) {
                 snprintf(buffer, 1000, "%s\n", message);
 
                 int n = strlen(buffer);
                 int nsend = 0;
 
-                while(n > 0)
-                {
+                while (n > 0) {
                     nsend = write(events[i].data.fd, buffer + nsend, n);
-                    if(nsend < 0 && errno != EAGAIN)
-                    {
+                    if (nsend < 0 && errno != EAGAIN) {
                         close(events[i].data.fd);
                         return 0;
                     }
@@ -143,106 +156,30 @@ int main(int argc, char** argv)
                 epollout = 1;
             }
 
-            if((events[i].events & EPOLLIN) && epollin == 0)
-            {
-                bzero(buffer,1000);
+            if ((events[i].events & EPOLLIN) && epollin == 0) {
+                bzero(buffer, 1000);
 
                 int n = 0;
                 int nrecv = 0;
 
-                while(1){
-                    nrecv = read(events[i].data.fd, buffer + n, 999) ;
-                    if(nrecv == -1 && errno != EAGAIN)
-                    {
+                while (1) {
+                    nrecv = read(events[i].data.fd, buffer + n, 999);
+                    if (nrecv == -1 && errno != EAGAIN) {
                         perror("read error!");
                     }
-                    if((nrecv == -1 && errno == EAGAIN) || nrecv == 0)
-                    {
+                    if ((nrecv == -1 && errno == EAGAIN) || nrecv == 0) {
                         break;
                     }
                     n += nrecv;
                 }
-                loop=1;
+                loop = 1;
                 epollin = 1;
                 printf("Response: %s\n", buffer);
             }
         }
     }
-    close(sk);
-    close(efd);
-    return 0;
-}
-
-/*
- * UTILS
- *
- * */
-
-//remove spaces and \n
-void rm_spaces(void *src) {
-    char *from = src;
-    char *to = src;
-
-    while (*from) {
-        if (*from != ' ' && *from != '\n') {
-            *to = *from;
-            to++;
-        }
-        from++;
-    }
-    *to = '\0';
-    return;
-}
-
-void rm_slash_n(void *src) {
-    char *from = src;
-    char *to = src;
-
-    while (*from) {
-        if (*from != '\n') {
-            *to = *from;
-            to++;
-        }
-        from++;
-    }
-    *to = '\0';
-    return;
-}
-
-
-int send_safe(int socket, void *buffer, size_t length) {
-    char *ptr = (char *) buffer;
-    while (length > 0) {
-        int i = send(socket, ptr, length, 0);
-        if (i < 1) {
-            return 1;
-        }
-        ptr += i;
-        length -= i;
-    }
-    return 0;
-}
-
-int get_safe(int socket_fd, char *message) {
-    char data[100];
-    ssize_t data_read;
-
-    while ((data_read = recv(socket_fd, data, 100, 0)) > 0) {
-        for (int i = 0; i < data_read; i++) {
-            *message++ = data[i];
-        }
-        if (data[data_read - 1] == '\t') {
-            break;
-        }
-        bzero(data, 100);
-    }
-
-    if (data_read == -1 && errno != 0) {
-        return 1;
-    }
-
-    *message = '\0';
-
+    close(epollfd);
+    close(fd);
     return 0;
 }
 
